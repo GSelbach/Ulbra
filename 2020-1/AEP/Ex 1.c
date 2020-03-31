4313160676bba2df470c40954d8ffed35334a0a9
GSelbach <gabriel.selbach@rede.ulbra.br>
2020-03-31 11:20:56 -0300

    att

---
+++ 2020-1/AEP/Ex 1.c
@@ -0,0 +1,19 @@
+// Faça um algoritmo para ler um número e informar se ele está na faixa de
+// números entre 100(inclusive) e 1000(inclusive).
+
+
+int main()
+{
+
+int numero;
+
+    printf ("Digite um número:");
+    scanf("%i", &numero);
+        if (numero >=100 && numero <=1000)
+        printf("O número escolhido está entre 100 e 1000");
+            else 
+            printf("O número não está entre 100 e 1000");
+            
+
+
+return 0;
---
+++ 2020-1/AEP/soma.c
@@ -0,0 +1,1 @@
+//função para somar dois numeros
